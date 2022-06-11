a002068272cd668b9aa285cd609e65d460227e65
Moussa Oumarou <oumar@Moussas-MacBook-Pro-2.local>
2022-06-11 23:09:22 +0200

    refactored YieldPrinter to use the proxy pattern to make it upgradeable

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,17 @@
 import * as dotenv from "dotenv"
 
-import { HardhatUserConfig, task } from "hardhat/config"
+import { HardhatUserConfig } from "hardhat/config"
 import "@nomiclabs/hardhat-etherscan"
 import "@nomiclabs/hardhat-waffle"
 import "@nomiclabs/hardhat-etherscan"
+import '@openzeppelin/hardhat-upgrades'
 import "@typechain/hardhat"
 import "hardhat-gas-reporter"
 import "solidity-coverage"
 
 dotenv.config()
 
-const { ETH_MAINNET_PK, INFURA_API_KEY, ETHERSCAN_API_KEY, ALCHEMY_API_KEY, POKT_PORTAL_ID } = process.env
+const { ETH_MAINNET_PK, INFURA_API_KEY, ETHERSCAN_API_KEY, ALCHEMY_API_KEY, POKT_PORTAL_ID, KOVAN_PK } = process.env
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
@@ -24,6 +25,10 @@ const config: HardhatUserConfig = {
     main: {
       url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [`0x${ETH_MAINNET_PK}`]
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`0x${KOVAN_PK}`]
     },
     localhost: {
       url: `http://127.0.0.1:8545`,
